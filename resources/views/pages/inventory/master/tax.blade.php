9b6ba40bd0ca47f8ad140f12e0e0ee09516e3a41
Ijlal Dhisa <ijlalwindhi15@gmail.com>
2024-11-19 15:03:08 +0700

    feat(inventory): update search input placeholder to include description

--- resources/views/pages/inventory/master/tax.blade.php
+++ resources/views/pages/inventory/master/tax.blade.php
@@ -7,7 +7,7 @@
             <div class="relative sm:w-full md:w-1/2 lg:w-2/6">
                 <input type="search" id="search-name"
                     class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-                    placeholder="Cari nama" />
+                    placeholder="Cari nama, deskripsi" />
                 <button type="button" id="search-button"
                     class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     <i class="fa-solid fa-magnifying-glass"></i>
