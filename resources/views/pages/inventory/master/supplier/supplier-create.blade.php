ee3566387f49f8ba1f516c6e4bac63f92e450b6d
Ijlal Dhisa <ijlalwindhi15@gmail.com>
2024-12-24 14:00:09 +0700

    fix(supplier): correct typo in supplier type option in create and edit forms

--- resources/views/pages/inventory/master/supplier/supplier-create.blade.php
+++ resources/views/pages/inventory/master/supplier/supplier-create.blade.php
@@ -17,7 +17,7 @@
                 <select name="type" id="type" required
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                     <option value="" selected disabled hidden>Pilih tipe supplier</option>
-                    <option value="Pedagang Besar Farmasi">Pedaan Besar Farmasi</option>
+                    <option value="Pedagang Besar Farmasi">Pedagang Besar Farmasi</option>
                     <option value="Apotek Lain">Apotek Lain</option>
                     <option value="Toko Obat">Toko Obat</option>
                     <option value="Lain-Lain">Lain-Lain</option>
