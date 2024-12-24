408ae180f17dfa8626c2a8dc677d29388b86c635
Ijlal Dhisa <ijlalwindhi15@gmail.com>
2024-12-24 13:54:41 +0700

    feat(supplier): update label from 'Kode' to 'Nama' in create and edit supplier forms

--- resources/views/pages/inventory/master/supplier/supplier-create.blade.php
+++ resources/views/pages/inventory/master/supplier/supplier-create.blade.php
@@ -5,7 +5,7 @@
         @csrf
         <div class="grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start justify-between gap-4">
             <div>
-                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kode
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
                     <span class="text-red-500">*</span></label>
                 <input type="text" name="name" id="name"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
