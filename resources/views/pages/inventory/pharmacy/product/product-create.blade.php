7ce2ca8a8f2c87ac61d4bb80d868ea83c7ac97c4
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-01-21 21:40:52 +0700

    refactor(product): update label and input names to minimum_smallest_stock for consistency

--- resources/views/pages/inventory/pharmacy/product/product-create.blade.php
+++ resources/views/pages/inventory/pharmacy/product/product-create.blade.php
@@ -75,11 +75,12 @@
                 </select>
             </div>
             <div>
-                <label for="minimum_stock" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stok
+                <label for="minimum_smallest_stock"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stok
                     Minimum (Satuan Terkecil)
                     <span class="text-red-500">*</span></label>
-                <input type="number" name="minimum_stock" id="minimum_stock" min="0" step="1"
-                    onkeypress="return event.charCode >= 48 && event.charCode <= 57"
+                <input type="number" name="minimum_smallest_stock" id="minimum_smallest_stock" min="0"
+                    step="1" onkeypress="return event.charCode >= 48 && event.charCode <= 57"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Stok Minimum" required>
             </div>
--- resources/views/pages/inventory/pharmacy/product/product-edit.blade.php
+++ resources/views/pages/inventory/pharmacy/product/product-edit.blade.php
@@ -71,11 +71,12 @@
                 </select>
             </div>
             <div>
-                <label for="minimum_stock" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stok
+                <label for="minimum_smallest_stock"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stok
                     Minimum (Satuan Terkecil)
                     <span class="text-red-500">*</span></label>
-                <input type="number" name="minimum_stock" id="minimum_stock" min="0" step="1"
-                    onkeypress="return event.charCode >= 48 && event.charCode <= 57"
+                <input type="number" name="minimum_smallest_stock" id="minimum_smallest_stock" min="0"
+                    step="1" onkeypress="return event.charCode >= 48 && event.charCode <= 57"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Stok Minimum" required>
             </div>
@@ -191,7 +192,8 @@
                     form.find('input[name="sku"]').val(data.sku);
                     form.find('select[name="type"]').val(data.type);
                     form.find('select[name="drug_group"]').val(data.drug_group);
-                    form.find('input[name="minimum_stock"]').val(data.minimum_stock);
+                    form.find('input[name="minimum_smallest_stock"]').val(data
+                        .minimum_smallest_stock);
                     form.find('select[name="supplier_id"]').val(data.supplier?.id);
                     form.find('select[name="is_active"]').val(data.is_active ? 'true' : 'false');
                     form.find('input[name="description"]').val(data.description);
