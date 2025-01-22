51282335f23d652a4cc56d7ecfcb27de16f65cc9
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-01-22 11:55:21 +0700

    feat(purchase-order): add unit handling in product purchase order creation and listing

--- app/Services/PurchaseOrderService.php
+++ app/Services/PurchaseOrderService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\PurchaseOrder;
 use App\Models\ProductPurchaseOrder;
 use App\Models\Product;
+use App\Models\Unit;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class PurchaseOrderService
@@ -19,17 +20,23 @@ class PurchaseOrderService
                 ProductPurchaseOrder::create([
                     'purchase_order_id' => $purchaseOrder->id,
                     'product_id' => $product['product'],
+                    'unit_id' => $product['unit'],
                     'qty' => $product['qty'],
                     'price' => $product['price'],
                     'discount' => $product['discount'],
                     'discount_type' => $product['discount_type'],
                     'subtotal' => $product['subtotal'],
-                    'description' => $product['description'] ?? null
                 ]);
 
                 // Update stok produk
                 $productModel = Product::find($product['product']);
-                $productModel->stock += $product['qty'];
+                if ($productModel->largest_unit == $product['unit']) {
+                    $productModel->largest_stock += $product['qty'];
+                    $productModel->smallest_stock += ($product['qty'] * $productModel->conversion_value);
+                } else {
+                    $productModel->smallest_stock += $product['qty'];
+                    $productModel->largest_stock += ($product['qty'] / $productModel->conversion_value);
+                }
                 $productModel->save();
             }
         }
@@ -70,7 +77,7 @@ class PurchaseOrderService
         $query->orderBy('created_at', 'desc');
 
         // Include relationships
-        $query->with(['supplier', 'tax', 'paymentType', 'productPurchaseOrders.product']);
+        $query->with(['supplier', 'tax', 'paymentType', 'productPurchaseOrders.product', 'productPurchaseOrders.unit']);
 
         return $query->paginate($perPage, ['*'], 'page', $page);
     }
