3abee04f44b96e68f0a1fbbde4f22430cbc69fe1
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-01-23 11:22:56 +0700

    feat(recipe): create Recipe model with soft deletes and relationships

---
+++ app/Models/Recipe.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Recipe extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'm_recipe';
+
+    protected $fillable = [
+        'name',
+        'status',
+        'staff_id',
+        'customer_name',
+        'customer_age',
+        'customer_address',
+        'doctor_name',
+        'doctor_sip',
+    ];
+
+    public function staff()
+    {
+        return $this->belongsTo(User::class, 'staff_id');
+    }
+}
