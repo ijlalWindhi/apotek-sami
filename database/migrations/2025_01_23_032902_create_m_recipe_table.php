654beb486523decdee7dceae781457ee6275ebdd
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-01-23 10:51:49 +0700

    feat(recipe): table recipe

---
+++ database/migrations/2025_01_23_032902_create_m_recipe_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_recipe', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('name');
+            $table->enum('status', ['Proses', 'Tunda']);
+            $table->foreignId('staff_id')->constrained('users')->onDelete('cascade');
+            $table->string('customer_name');
+            $table->integer('customer_age');
+            $table->string('customer_address');
+            $table->string('doctor_name');
+            $table->string('doctor_sip');
+            $table->index(['staff_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_recipe');
+    }
+};
