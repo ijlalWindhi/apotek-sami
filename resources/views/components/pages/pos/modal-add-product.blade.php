7c9d80a76242444b53a18adfbaacc7942112a85f
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-01-28 16:15:11 +0700

    feat(pos): adjust modal product table to improve overflow handling and enhance user interface

--- resources/views/components/pages/pos/modal-add-product.blade.php
+++ resources/views/components/pages/pos/modal-add-product.blade.php
@@ -29,7 +29,7 @@
                     </button>
                 </div>
             </div>
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg pb-6">
+            <div class="relative overflow-auto shadow-md sm:rounded-lg pb-6 max-h-[50vh]">
                 <table class="w-full text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
