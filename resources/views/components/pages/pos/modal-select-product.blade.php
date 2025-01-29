b0c25104db0433b1af5931005a632548b15a8a72
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-01-29 16:09:40 +0700

    fix(pos): add unique IDs to product name table cells for better accessibility

--- resources/views/components/pages/pos/modal-select-product.blade.php
+++ resources/views/components/pages/pos/modal-select-product.blade.php
@@ -204,7 +204,7 @@
 
         tableRowProduct: (product) => `
             <tr id="list_product_pos_${product.id}" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <td class="px-3 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <td class="px-3 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white" id="product_pos_name_${product.id}">
                     ${utils.escapeHtml(product.name || '-')}
                 </td>
                 <td class="px-3 py-2 text-gray-500 dark:text-gray-400">
