5a9d0ce6fbb11d6e9d957b474430ddcf383335f8
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-01-29 12:06:52 +0700

    feat(pos): add ProductTransaction model for managing product transactions

---
+++ app/Models/ProductTransaction.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductTransaction extends Model
+{
+    use SoftDeletes;
+    const DECIMAL_FORMAT = 'decimal:2';
+
+    protected $table = 'm_product_transaction';
+
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'unit_id',
+        'qty',
+        'price',
+        'tuslah',
+        'discount',
+        'discount_type',
+        'subtotal',
+    ];
+
+    protected $casts = [
+        'qty' => 'integer',
+        'price' => self::DECIMAL_FORMAT,
+        'tuslah' => self::DECIMAL_FORMAT,
+        'discount' => self::DECIMAL_FORMAT,
+        'subtotal' => self::DECIMAL_FORMAT
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transaction_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class, 'unit_id');
+    }
+}
