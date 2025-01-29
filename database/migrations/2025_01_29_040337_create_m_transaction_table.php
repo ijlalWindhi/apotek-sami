d34de11522262d4a1c8d7aca75d2f75e377a7fd5
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-01-29 12:07:06 +0700

    feat(pos): add migration for m_transaction table to manage transaction records

---
+++ database/migrations/2025_01_29_040337_create_m_transaction_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_transaction', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->enum('customer_type', ['Umum', 'Rutin', 'Karyawan'])->default('Umum');
+            $table->foreignId('recipe_id')->nullable()->constrained('m_recipe')->nullOnDelete();
+            $table->text('notes')->nullable();
+            $table->foreignId('payment_type_id')->constrained('m_payment_type')->onDelete('cascade');
+            $table->enum('status', ['Terbayar', 'Belum Lunas', 'Tertunda'])->default('Terbayar');
+            $table->string('invoice_number')->unique();
+            $table->decimal('discount', 12, 2)->default(0);
+            $table->enum('discount_type', ['Percentage', 'Nominal'])->default('Percentage');
+            $table->decimal('paid_amount', 12, 2);
+            $table->decimal('change_amount', 12, 2);
+            $table->decimal('total_amount', 12, 2);
+            $table->foreignId('created_by')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_transaction');
+    }
+};
