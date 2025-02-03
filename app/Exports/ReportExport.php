7d4a796e32ad52f4d73add7d49ce3a326b0ca7d5
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-02-03 14:12:15 +0700

    feat(report): add ReportExport class for Excel export functionality

---
+++ app/Exports/ReportExport.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class ReportExport implements FromArray, WithHeadings, WithStyles
+{
+    protected $report;
+
+    public function __construct(array $report)
+    {
+        $this->report = $report;
+    }
+
+    public function array(): array
+    {
+        return [
+            [
+                'Penjualan',
+                'Rp ' . number_format($this->report['penjualan'], 2, ',', '.')
+            ],
+            [
+                'Tuslah',
+                'Rp ' . number_format($this->report['tuslah'], 2, ',', '.')
+            ],
+            [
+                'Diskon Penjualan',
+                'Rp ' . number_format($this->report['diskon_penjualan'], 2, ',', '.')
+            ],
+            [
+                'Retur Penjualan',
+                'Rp ' . number_format($this->report['retur_penjualan'], 2, ',', '.')
+            ],
+            [
+                'Penjualan Bersih',
+                'Rp ' . number_format($this->report['penjualan_bersih'], 2, ',', '.')
+            ],
+            [
+                'Harga Pokok Pembelian',
+                'Rp ' . number_format($this->report['harga_pokok_pembelian'], 2, ',', '.')
+            ],
+            [
+                'Laba Kotor',
+                'Rp ' . number_format($this->report['laba_kotor'], 2, ',', '.')
+            ],
+            [
+                'Penyesuaian Stock',
+                'Rp ' . number_format($this->report['penyesuaian_stock'], 2, ',', '.')
+            ],
+            [
+                'Keuntungan Apotek',
+                'Rp ' . number_format($this->report['keuntungan_apotek'], 2, ',', '.')
+            ],
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Keterangan',
+            'Nominal'
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => ['font' => ['bold' => true]],
+            'A' => ['font' => ['bold' => true]],
+            'B' => ['alignment' => ['horizontal' => 'right']],
+        ];
+    }
+}
