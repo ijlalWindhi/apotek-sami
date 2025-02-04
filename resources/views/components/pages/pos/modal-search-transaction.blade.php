a5b6fc4aaf8343c11febf4b37c54d2b472b00ec7
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-02-04 20:00:57 +0700

    feat(transaction): change return button to link for transaction view

--- resources/views/components/pages/pos/modal-search-transaction.blade.php
+++ resources/views/components/pages/pos/modal-search-transaction.blade.php
@@ -177,14 +177,16 @@
                     >
                         <i class="fa-solid fa-pencil"></i>
                     </x-button>
-                    <x-button
-                        id="btn-return-transaction-${transaction.id}"
-                        data-id="${transaction.id}"
-                        color="red"
-                        size="sm"
-                    >
-                        <i class="fa-solid fa-rotate-left mr-2"></i> Return
-                    </x-button>
+                    <a href="/pos/return/view/${transaction.id}">
+                        <x-button
+                            id="btn-return-transaction-${transaction.id}"
+                            data-id="${transaction.id}"
+                            color="red"
+                            size="sm"
+                        >
+                            <i class="fa-solid fa-rotate-left mr-2"></i> Return
+                        </x-button>
+                    </a>
                 </td>
             </tr>
         `,
