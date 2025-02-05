ec93aeb0838ff5a17a6df0e4bbd57af2ad5a3520
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-02-05 10:32:05 +0700

    feat(return): add ProductReturnResource for transforming product return data

---
+++ app/Http/Resources/ProductReturnResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductReturnResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'return' => new ReturnResource($this->whenLoaded('retur')),
+            'product_transaction' => new ProductTransactionResource($this->whenLoaded('productTransaction')),
+            'product' => new ProductResource($this->whenLoaded('product')),
+            'unit' => new UnitResource($this->whenLoaded('unit')),
+            'qty_return' => $this->qty_return,
+            'subtotal_return' => $this->subtotal_return,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'deleted_at' => $this->deleted_at?->format('Y-m-d H:i:s')
+        ];
+    }
+}
