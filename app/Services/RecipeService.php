4ad54f95fca04e3d427c2628765563af8797ea4c
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-02-06 11:18:24 +0700

    feat(recipe): implement update functionality for recipes with product management

--- app/Services/RecipeService.php
+++ app/Services/RecipeService.php
@@ -68,4 +68,72 @@ class RecipeService
     {
         return $recipe->delete();
     }
+
+    public function update(Recipe $recipe, array $data): Recipe
+    {
+        try {
+            return DB::transaction(function () use ($recipe, $data) {
+                // Update recipe data
+                $recipe->update([
+                    'name' => $data['name'],
+                    'staff_id' => $data['staff_id'],
+                    'customer_name' => $data['customer_name'],
+                    'customer_age' => $data['customer_age'],
+                    'customer_address' => $data['customer_address'],
+                    'doctor_name' => $data['doctor_name'],
+                    'doctor_sip' => $data['doctor_sip'],
+                ]);
+
+                // Get existing product recipes
+                $existingProductRecipes = $recipe->productRecipes()->pluck('id')->toArray();
+
+                // Initialize array to track product recipes to be deleted
+                $productRecipesToDelete = $existingProductRecipes;
+
+                // Process each product in the request
+                if (!empty($data['products'])) {
+                    foreach ($data['products'] as $product) {
+                        if (isset($product['id'])) {
+                            // Update existing product recipe
+                            $recipe->productRecipes()->where('id', $product['id'])->update([
+                                'product_id' => $product['product'],
+                                'unit_id' => $product['unit'],
+                                'qty' => $product['qty'],
+                                'price' => $product['price'],
+                                'tuslah' => $product['tuslah'],
+                                'discount' => $product['discount'],
+                                'discount_type' => $product['discount_type'],
+                                'subtotal' => $product['subtotal'],
+                            ]);
+
+                            // Remove this ID from the delete list
+                            $productRecipesToDelete = array_diff($productRecipesToDelete, [$product['id']]);
+                        } else {
+                            // Add new product recipe
+                            $recipe->productRecipes()->create([
+                                'product_id' => $product['product'],
+                                'unit_id' => $product['unit'],
+                                'qty' => $product['qty'],
+                                'price' => $product['price'],
+                                'tuslah' => $product['tuslah'],
+                                'discount' => $product['discount'],
+                                'discount_type' => $product['discount_type'],
+                                'subtotal' => $product['subtotal'],
+                            ]);
+                        }
+                    }
+                }
+
+                // Delete product recipes that are no longer in the request
+                if (!empty($productRecipesToDelete)) {
+                    $recipe->productRecipes()->whereIn('id', $productRecipesToDelete)->delete();
+                }
+
+                return $recipe;
+            });
+        } catch (\Exception $e) {
+            Log::error('Failed to update recipe: ' . $e->getMessage());
+            throw $e;
+        }
+    }
 }
